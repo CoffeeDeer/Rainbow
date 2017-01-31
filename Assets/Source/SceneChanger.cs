57504a0c4e9293f9a49cb341b2defe2337be6554
rubi1541 <rubi1541@naver.com>
2017-01-31 22:51:17 +0900

    FadeoutEffect
    FadeoutScreen block the Cube Click

---
+++ Assets/Source/SceneChanger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SceneChanger : MonoBehaviour {
+
+	public GameObject FadeOutBlackImage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartSceneFadeoutEffect(){
+		StartCoroutine (SceneFadeOut ());
+	}
+
+	IEnumerator SceneFadeOut(){
+		const int loop = 30;
+
+		Image fadeoutImage = FadeOutBlackImage.GetComponent<Image> ();
+		Color color = fadeoutImage.color;
+
+		for (int i = 0; i < loop; i++) {
+			color.a = (i+1) / (float)loop;
+			fadeoutImage.color = color;
+			yield return new WaitForSeconds(0.03f);
+		}
+
+	}
+}
